971e3ae5d2342bfa1aeb128965953ad3955ffcb7
TangNULL <1358336145@qq.com>
2022-06-25 15:15:21 +0800

    修改build.sh

--- libxml2-2.9.12_github_version/build.sh
+++ libxml2-2.9.12_github_version/build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-rm -rf CMakeCache.txt
-make clean
+rm -f CMakeCache.txt 
 cmake -DSTEP="build"
 make
+cd rust_ffi
+cargo clean
+cd ..
 cd rust
 cargo clean
-cargo build --release
-cd ../
+cargo build
+cd ..
 cmake -DSTEP="link"
 make
+ctest
