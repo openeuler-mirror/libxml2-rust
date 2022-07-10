feed711734a7ddc8c69c1413bdc1821786ae2014
tqq1997 <1358336145@qq.com>
2022-07-10 10:14:23 +0000

    modify

--- libxml2-2.9.12_github_version/rust_ffi/target/rls/debug/deps/rust_ffi-335beca794b781cc.d
+++
@@ -1,5 +0,0 @@
-/root/pr/20220710_1/libxml2-rust_1/libxml2-2.9.12_github_version/rust_ffi/target/rls/debug/deps/rust_ffi-335beca794b781cc.rmeta: src/lib.rs
-
-/root/pr/20220710_1/libxml2-rust_1/libxml2-2.9.12_github_version/rust_ffi/target/rls/debug/deps/rust_ffi-335beca794b781cc.d: src/lib.rs
-
-src/lib.rs:
--- libxml2-2.9.12_github_version/rust_ffi/target/rls/debug/deps/rust_ffi-8c3fdbaabe355afb.d
+++
@@ -1,5 +0,0 @@
-/root/pr/20220710_1/libxml2-rust_1/libxml2-2.9.12_github_version/rust_ffi/target/rls/debug/deps/rust_ffi-8c3fdbaabe355afb.rmeta: src/lib.rs
-
-/root/pr/20220710_1/libxml2-rust_1/libxml2-2.9.12_github_version/rust_ffi/target/rls/debug/deps/rust_ffi-8c3fdbaabe355afb.d: src/lib.rs
-
-src/lib.rs:
