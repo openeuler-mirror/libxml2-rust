b1cb00b3bf64dfbc0cda42e986398168b2bee7d9
smvk <963173105@qq.com>
2022-07-23 14:26:51 +0000

    modify

--- ci/01-base.sh
+++ ci/01-base.sh
@@ -2,28 +2,28 @@
 
 cd ./libxml2-2.9.12_github_version/rust
 
-# #开始检查
-# cargo fmt --all -- --check -v
-# cargo clean
+#开始检查
+cargo fmt --all -- --check -v
+cargo clean
 
-# #cargo clippy --all-targets --all-features --tests --benches -- -D warnings
-# cargo clippy --all-targets --all-features --tests --benches -- -v
-# cargo clean
+#cargo clippy --all-targets --all-features --tests --benches -- -D warnings
+cargo clippy --all-targets --all-features --tests --benches -- -v
+cargo clean
 
-# cargo check
-# cargo clean
+cargo check
+cargo clean
 
-# #cargo rustc -- -D warnings
-# bin=$(sed -n '/[[bin]]/ {n;p}' Cargo.toml | sed 's/\"//g' | sed 's/name = //g')
-# for bin_name in $bin
-# do
-# echo $bin_name
-# cargo rustc --bin $bin_name -- -D warnings -v
-# done
+#cargo rustc -- -D warnings
+bin=$(sed -n '/[[bin]]/ {n;p}' Cargo.toml | sed 's/\"//g' | sed 's/name = //g')
+for bin_name in $bin
+do
+echo $bin_name
+cargo rustc --bin $bin_name -- -D warnings -v
+done
 
-# cargo build --release -v
+cargo build --release -v
 
-# #RUST_BACKTRACE=1 cargo test --all -v -- --nocapture --test-threads=1
-# RUST_BACKTRACE=1 cargo test --all -- --nocapture
+#RUST_BACKTRACE=1 cargo test --all -v -- --nocapture --test-threads=1
+RUST_BACKTRACE=1 cargo test --all -- --nocapture
 
-# cargo doc --all --no-deps
+cargo doc --all --no-deps
