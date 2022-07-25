c3eb1a5a6828ac308593d90c2dc7b69e78c826ac
smvk <963173105@qq.com>
2022-07-25 13:25:44 +0000

    modify

--- ci/00-pre.sh
+++ ci/00-pre.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo yum clean all
-sudo yum install -y openssl-libs
+sudo yum install -y gcc  openssl-libs
 
-sudo yum install -y gcc make cmake
+sudo yum install -y make cmake
 sudo yum install -y libxml2-devel python3-devel zlib-devel bzip2-devel
 
 #git加速并安装rust工具链
