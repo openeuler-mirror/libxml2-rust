76bb15e1fe0b04ae77d91a6b037c3c68e944ffb3
smvk <963173105@qq.com>
2022-07-25 10:44:51 +0000

    modify

--- ci/00-pre.sh
+++ ci/00-pre.sh
@@ -3,7 +3,7 @@ sudo yum clean all
 sudo yum install -y openssl-libs
 
 sudo yum install -y gcc g++ make cmake
-sudo yum install -y libxml2-devel python3-devel
+sudo yum install -y libxml2-devel zlib-devel bzip2-devel python3-devel
 
 #git加速并安装rust工具链
 git config --global url."https://github.91chi.fun/https://github.com/".insteadOf "https://github.com/"
