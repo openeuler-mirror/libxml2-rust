0bc8d02eba71e71f93859f017d2e797032abc816
kevinchannn <779834766@qq.com>
2022-07-31 07:46:31 +0800

    modify

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -8,6 +8,7 @@ include!("ffi_safe.rs");
 include!("parserInternals.rs");
 include!("parserInternals_rust.rs");
 include!("parser.rs");
+include!("parser_part.rs");
 include!("parser_rust.rs");
 include!("HTMLparser.rs");
 include!("HTMLparser_rust.rs");
