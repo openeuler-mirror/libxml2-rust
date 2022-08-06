1226cabfb380967e2f990ba72ac6e8df1fe7e78f
kevinchannn <779834766@qq.com>
2022-08-06 20:07:17 +0800

    modify

---
+++ CMakeFiles/testdict.dir/testdict.c.o.d
@@ -0,0 +1,56 @@
+CMakeFiles/testdict.dir/testdict.c.o: \
+ /root/code/01/08/libxml2-rust/testdict.c /usr/include/stdc-predef.h \
+ /usr/include/string.h /usr/include/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/lib/gcc/x86_64-redhat-linux/8/include/stddef.h \
+ /usr/include/bits/types/locale_t.h /usr/include/bits/types/__locale_t.h \
+ /usr/include/strings.h \
+ /root/code/01/08/libxml2-rust/include/libxml/parser.h \
+ /usr/lib/gcc/x86_64-redhat-linux/8/include/stdarg.h \
+ /root/code/01/08/libxml2-rust/libxml/xmlversion.h \
+ /root/code/01/08/libxml2-rust/include/libxml/xmlexports.h \
+ /root/code/01/08/libxml2-rust/include/libxml/tree.h /usr/include/stdio.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/bits/types/__fpos_t.h /usr/include/bits/types/__mbstate_t.h \
+ /usr/include/bits/types/__fpos64_t.h /usr/include/bits/types/__FILE.h \
+ /usr/include/bits/types/FILE.h /usr/include/bits/types/struct_FILE.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
+ /usr/lib/gcc/x86_64-redhat-linux/8/include/limits.h \
+ /usr/lib/gcc/x86_64-redhat-linux/8/include/syslimits.h \
+ /usr/include/limits.h /usr/include/bits/posix1_lim.h \
+ /usr/include/bits/local_lim.h /usr/include/linux/limits.h \
+ /usr/include/bits/posix2_lim.h \
+ /root/code/01/08/libxml2-rust/include/libxml/xmlstring.h \
+ /root/code/01/08/libxml2-rust/include/libxml/xmlregexp.h \
+ /root/code/01/08/libxml2-rust/include/libxml/dict.h \
+ /root/code/01/08/libxml2-rust/include/libxml/hash.h \
+ /root/code/01/08/libxml2-rust/include/libxml/valid.h \
+ /root/code/01/08/libxml2-rust/include/libxml/xmlerror.h \
+ /root/code/01/08/libxml2-rust/include/libxml/list.h \
+ /root/code/01/08/libxml2-rust/include/libxml/xmlautomata.h \
+ /root/code/01/08/libxml2-rust/include/libxml/entities.h \
+ /root/code/01/08/libxml2-rust/include/libxml/encoding.h \
+ /usr/include/iconv.h \
+ /root/code/01/08/libxml2-rust/include/libxml/xmlIO.h \
+ /root/code/01/08/libxml2-rust/include/libxml/globals.h \
+ /root/code/01/08/libxml2-rust/include/libxml/SAX2.h \
+ /usr/include/stdlib.h /usr/include/bits/waitflags.h \
+ /usr/include/bits/waitstatus.h /usr/include/bits/floatn.h \
+ /usr/include/bits/floatn-common.h /usr/include/sys/types.h \
+ /usr/include/bits/types/clock_t.h /usr/include/bits/types/clockid_t.h \
+ /usr/include/bits/types/time_t.h /usr/include/bits/types/timer_t.h \
+ /usr/include/bits/stdint-intn.h /usr/include/endian.h \
+ /usr/include/bits/endian.h /usr/include/bits/byteswap.h \
+ /usr/include/bits/uintn-identity.h /usr/include/sys/select.h \
+ /usr/include/bits/select.h /usr/include/bits/types/sigset_t.h \
+ /usr/include/bits/types/__sigset_t.h \
+ /usr/include/bits/types/struct_timeval.h \
+ /usr/include/bits/types/struct_timespec.h \
+ /usr/include/bits/pthreadtypes.h /usr/include/bits/thread-shared-types.h \
+ /usr/include/bits/pthreadtypes-arch.h /usr/include/alloca.h \
+ /usr/include/bits/stdlib-float.h \
+ /root/code/01/08/libxml2-rust/include/libxml/xlink.h \
+ /root/code/01/08/libxml2-rust/include/libxml/xmlmemory.h \
+ /root/code/01/08/libxml2-rust/include/libxml/threads.h
